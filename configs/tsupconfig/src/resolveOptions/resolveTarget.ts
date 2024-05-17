c9564e0d7d6291399fe91f7710d5352289d32b55
jedlinsky <info@jedlinsky.me>
2024-05-17 19:21:28 +0200

    fix(tsupconfig): ternary operation priority in resolveTarget function

--- configs/tsupconfig/src/resolveOptions/resolveTarget.ts
+++ configs/tsupconfig/src/resolveOptions/resolveTarget.ts
@@ -1,7 +1,7 @@
-import type { ResolveTarget } from './types.js'
+import type { ResolveTarget, Target } from './types.js'
 
 const resolveTarget: ResolveTarget = function ({ isBrowser, tsConfig }) {
-  return tsConfig.compilerOptions?.target ?? isBrowser ? 'es2020' : 'node19'
+  return (tsConfig.compilerOptions?.target as Target | undefined) ?? (isBrowser ? 'es2020' : 'node19')
 }
 
 export { resolveTarget }
